896a0bcf2d2ab0f1cca50d5ee4c754923c491232
DimaKrupenko <dimakrupenko77@gmail.com>
2022-12-11 15:40:17 +0200

    upd: работаю над 3 заданием

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,15 @@
 import css from './FriendList.module.css'
-import PropTypes from 'prop-types'; 
+// import PropTypes from 'prop-types'; 
 
 export const FriendList = ({ friends }) => {
- return   <ul className="friend-list">
+    return <ul className={css.friendList}>
      {friends.map(friend => (
-         <li key={friend.id}className="item">
-          <span className="status">{friend.isOnline}</span>
+         <li key={friend.id} className={css.item}>
+             <span className={css.status}>{friend.isOnline}</span>
   <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
           <p className="name">{friend.name}</p>
 </li>
     ))}
 </ul>
 }
+
